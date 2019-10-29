391d12ff2a8e1b17d1292da2902264a944097fe0
UzayPoyraz <54606964+UzayPoyraz@users.noreply.github.com>
2019-10-29 09:19:42 +0900

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This program uninstalls the application
+echo "Are you sure you want to uninstalls CarRentalApp? press y:Yes or n:No"
+read ans
+if [ $ans == y ]; then
+  echo "Uninstalling file.."
+  rm -r ~/Desktop/CarRentalApp/
+  echo "Uninstalled succesfully."
+else
+  echo "The file is not uninstalled."
+  exit
+fi
